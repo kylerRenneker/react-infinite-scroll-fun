ea33b75330f250ec899de89014c2c2820aa70479
Kyler <Krenneker16@gmail.com>
2019-11-01 00:39:40 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
 
 function App() {
+  const [hasMore, setHasMore] = useState(true)
+  const [jokes, setJokes] = useState([])
+  const [pageNumber, setPageNumber] = useState(1)
+  const [totalPages, setTotalPages] = useState(null)
+
+  const getJokes = () => {
+    fetch(`https://icanhazdadjoke.com/search?page=${pageNumber}`, {
+      headers: {
+        accept: "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setTotalPages(data.total_pages)
+        let jokeState = jokes;
+        data.results.map((joke) => {
+          jokeState.push(joke)
+        })
+        setJokes(jokeState)
+      })
+      .then(res => {
+        if (pageNumber === totalPages) {
+          setHasMore(false)
+
+        } else {
+          setPageNumber(pageNumber + 1);
+        }
+      })
+  }
+
+  const renderJokes = () => {
+    let items = []
+
+    jokes.map((joke, i) => items.push(<h1 key={i}>{joke.joke}</h1>))
+
+    return items;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={getJokes}
+        hasMore={hasMore}
+        loader={<div className="loader" key={0}>Loading ...</div>}
+      >
+        {renderJokes()}
+        {pageNumber === totalPages ? <h1>Sorry, no more jokes...</h1> : null}
+      </InfiniteScroll>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
